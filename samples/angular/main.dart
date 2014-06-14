1714441d59e9221b75412c215ac5f836126249b0
ggirou <porude-git@yahoo.fr>
2014-06-14 12:00:24 +0200

    Start examples for Module

--- samples/angular/main.dart
+++ samples/angular/main.dart
@@ -2,9 +2,22 @@ import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
 main() {
-  applicationFactory().run();
+  applicationFactory().addModule(new MyAppModule()).run();
 }
 
 dblClickHandler() {
   print("dblClickHandler");
+}
+
+class MyAppModule extends Module {
+  MyAppModule() {
+    install(new AnotherModule());
+    // TODO: working examples
+    //    bind(TypeToBind);
+    //    bindByKey(KeyToBind);
+  }
+}
+class AnotherModule extends Module {
+  AnotherModule() {
+  }
 }
