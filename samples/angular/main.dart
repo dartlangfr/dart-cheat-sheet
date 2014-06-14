cfeaa0c69650c54de4eee28998cb83449ba5d396
ggirou <porude-git@yahoo.fr>
2014-06-14 21:27:41 +0200

    Add decorator and  Controller examples

--- samples/angular/main.dart
+++ samples/angular/main.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
@@ -12,12 +13,37 @@ dblClickHandler() {
 class MyAppModule extends Module {
   MyAppModule() {
     install(new AnotherModule());
+    bind(RecipeBookController);
+    bind(Tooltip);
     // TODO: working examples
-    //    bind(TypeToBind);
     //    bindByKey(KeyToBind);
   }
 }
 class AnotherModule extends Module {
   AnotherModule() {
   }
+}
+
+@Decorator(
+  selector: '[tooltip]', 
+  map: const {
+    'tooltip': '@text'
+  })
+class Tooltip {
+  final Element element;
+
+  String text;
+
+  Tooltip(this.element) {
+    element
+        ..onMouseEnter.listen((_) => element.text = "Tooltip: $text")
+        ..onMouseLeave.listen((_) => element.text = "Tooltip");
+  }
+}
+
+@Controller(
+    selector: '[recipe-book]',
+    publishAs: 'ctrl')
+class RecipeBookController {
+  String data = "Hello World!";
 }
